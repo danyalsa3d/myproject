902a027523a2a7c982582fdf308e7ace297d1049
danyalsa3d <danyalsa3d@gmail.com>
2022-09-29 08:07:07 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<html lang="ar" dir="rtl">
+<head>
+    <title>لوحة التحكم</title>
+</head>
+
+<body>
+<center>
+    <br><br>
+    <h1>صفحات لوحة التحكم </h1>
+    <br><br>
+
+    <form method="post" action ="loginpage.php">
+    <input type="submit" name="login_b" value="تسجيل دخول" style="width: 15%;" />
+    </form>
+    <form method="post" action ="controll_pages/allusers.php" >
+        <input type="submit" name="allusers_b" value="الموظفين" style="width: 15%;" />
+    </form>
+    <form method="post" action ="controll_pages/allcov.php">
+        <input type="submit" name="allcov_b" value="العهد" style="width: 15%;" />
+    </form>
+</center>
+
+</body>
+
+
+
+
+<footer></footer>
+</html>
